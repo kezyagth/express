c55571ca7a3a5a2df74e1cd5c2cfeee89f9eeaba
kezyagth <kezyagatha@gmail.com>
2019-10-29 14:47:35 +0700

    get detail by id

--- mongoModel/mongoConfig.js
+++ mongoModel/mongoConfig.js
@@ -3,10 +3,3 @@ Mongoose.connect('mongodb://localhost/belajarmongo'); //mengkoneksikan ke db mon
 module.exports = Mongoose; //export module mongoose
 //commit -m "Config mongo dan koneksi ke db"
 
-const Mongoose = require('./mongoModel/mongoConfig')
-const PersonModel = Mongoose.model("person", {
-    firstname: String, //field firstname
-    lastname: String //field lastname
-});
-//commit -m "Memanggil Mongoconfig dan membuat model PersonModel sebagai penampung collection person"
-app.get('/',(req, res) => res.send('Hello World'))
--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express()
 const port = 3000
 //memanggil library body-parser
 const bodyParser = require("body-parser");
+
+const Mongoose = require("./mongoModel/mongoConfig")
+const PersonModel = Mongoose.model("person",{
+    firstname: String,
+    lastname: String
+})
+
 //config body-parser
 app.use(bodyParser.urlencoded({extended:true})); //menangkap tipe urlencode dalam bentuk body-parser
 app.use(bodyParser.json()); // menagkap url dalam bentuk json
@@ -22,5 +29,52 @@ app.post('/hello', function(req,res){
     res.json(respon);
 })
 // commit lagi dengan nama "membuat request post" lalu push
+
+app.post('/profile/create', async (req, res) => {
+
+    //Do something here
+    console.log(req.body)
+    const insert = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname
+    }
+    var person = new PersonModel(insert);
+    var result = await person.save();
+    const response = {
+        statusCode: 200,
+        error: "",
+        message: "Create Person",
+        content: result
+    }
+    res.json(response);
+})
+//menampilkan semua data
+// url http://localhost:3000/profile/list
+app.get('/profile/list', async (req, res) => {
+    var person = await PersonModel.find().exec();
+    const response = {
+        statusCode : 200,
+        error: "",
+        message: "List Person",
+        content: person
+    }
+    res.json(response);
+})
+
+//default profil data method get
+//http://localhost:3000/profile/detail/idmmongo
+app.get('/profile/detail/(:id)', async (req, res) => {
+    let statusCode= 200
+    let message= "Detail Person"
+    var person = await PersonModel.findById(req.params.id).exec();
+    const response = {
+        statusCode: 200,
+        error: message,
+        message:message,
+        content: person
+    }
+    res.status(statusCode).json(response);
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!` ))
 
